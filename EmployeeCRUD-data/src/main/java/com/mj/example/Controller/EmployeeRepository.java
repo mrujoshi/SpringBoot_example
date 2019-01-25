4245d78aab59e72900d7841620bd61853c05492f
Mrudula Joshi <35089282+mrujoshi@users.noreply.github.com>
2019-01-25 11:12:35 +0530

    Delete EmployeeRepository.java

--- EmployeeCRUD-data/src/main/java/com/mj/example/Controller/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mj.example.Controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-}
